faef511893172e45536cb612a10a133f5ec8dc7d
Ungarscool1 <ungarscool1@users.noreply.github.com>
2017-04-15 21:28:30 +0200

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+server_scripts {
+	'inv_sv.lua',
+	'phone/phone_sv.lua',
+	} 
+
+client_scripts {
+	'GUI.lua',
+	'inv.lua',
+  'phone/phone_cl.lua',
+	}
